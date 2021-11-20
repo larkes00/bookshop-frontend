847824bab32a6970928f62b78daa01a17520ac54
larkes00 <zakirov2802@gmail.com>
2021-11-21 01:36:47 +0200

    Routing setting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HeaderComponent from "./component/HeaderComponent";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import BookListComponent from "./component/BookListComponent";
+import LoginComponent from "./component/LoginComponent";
+import RegistrationComponent from "./component/RegistrationComponent";
 
 function App() {
   return (
     <div className="App">
-      <HeaderComponent/>
+        <Router>
+            <HeaderComponent/>
+            <Switch>
+                <Route exact path="/" component={BookListComponent}/>
+                <Route exact path="/login" component={LoginComponent}/>
+                <Route exact path="/registration" component={RegistrationComponent}/>
+            </Switch>
+        </Router>
     </div>
   );
 }
