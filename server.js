af9e9556f878476d643dd9a714fc52dae50be903
larkes00 <zakirov2802@gmail.com>
2021-11-27 16:38:53 +0200

    Build settings fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname, 'build')))
 
-app.get('*', (res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
